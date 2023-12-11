05242bbfc0b704b7e98e665b5c0882f64f811906
MauzerovPR <szutemamazu@onet.pl>
2023-12-11 04:45:08 +0100

    add quarters

--- procedures/HowMuchDidSthEarned.sql
+++ procedures/HowMuchDidSthEarned.sql
@@ -1,32 +1,38 @@
+CREATE OR REPLACE FUNCTION QuarterYear(d TIMESTAMP)
+RETURNS TEXT AS $$ BEGIN
+	RETURN (extract(year from d)::text || '.Q' || extract(quarter from d)::text);
+END $$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION HowMuchEachFlightEarned()
-RETURNS TABLE (id BIGINT, earning DECIMAL(20, 2)) AS $$ BEGIN
+RETURNS TABLE (id BIGINT, earning DECIMAL(20, 2), quarter TEXT) AS $$ BEGIN
     RETURN QUERY
-    SELECT ticket.flight_id as id, SUM(ticket.cost)
+    SELECT ticket.flight_id as id, SUM(ticket.cost), QuarterYear(departure_time) as quarter
     FROM ticket
-    GROUP BY ticket.flight_id
+	INNER JOIN flight USING (flight_id)
+    GROUP BY ticket.flight_id, quarter
     ORDER BY ticket.flight_id;
 END $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION HowMuchEachPlaneEarned()
-RETURNS TABLE (id BIGINT, earning DECIMAL(20, 2)) AS $$ BEGIN
+RETURNS TABLE (id BIGINT, earning DECIMAL(20, 2), quarter TEXT) AS $$ BEGIN
     RETURN QUERY
-    SELECT flight.aircraft_id, SUM(ticket.cost)
+    SELECT flight.aircraft_id, SUM(ticket.cost), QuarterYear(departure_time) as quarter
     FROM ticket
     INNER JOIN flight USING (flight_id)
-    GROUP BY flight.aircraft_id
+    GROUP BY flight.aircraft_id, quarter
     ORDER BY flight.aircraft_id;
 END $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION HowMuchEachAirportEarned(with_origin BOOLEAN default True, with_destination BOOLEAN default True)
-RETURNS TABLE (id BIGINT, earning DECIMAL(20, 2)) AS $$ BEGIN
+RETURNS TABLE (id BIGINT, earning DECIMAL(20, 2), quarter TEXT) AS $$ BEGIN
     RETURN QUERY
-    SELECT airport, SUM(ticket.cost)
+    SELECT airport, SUM(ticket.cost), QuarterYear(departure_time) as quarter
     FROM ticket
     INNER JOIN (
-        SELECT flight_id, origin as airport FROM flight WHERE with_origin = True
-        UNION ALL SELECT flight_id, destination FROM flight WHERE with_destination = True
-    ) airports USING (flight_id) -- ON ticket.flight_id = airport
-    GROUP BY airport
+        SELECT flight_id, origin as airport, departure_time  FROM flight WHERE with_origin = True
+        UNION ALL SELECT flight_id, destination, departure_time FROM flight WHERE with_destination = True
+    ) airports USING (flight_id)
+    GROUP BY airport, quarter
     ORDER BY airport;
 END $$ LANGUAGE plpgsql;
 
